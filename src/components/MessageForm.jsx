e7c30df9c77e2028b0305bb5430a49e0f4f54db9
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-08-11 12:30:47 +0530

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -13,7 +13,7 @@ function MessageForm() {
     const handleMessage = async (e) => {
         e.preventDefault();
         try {
-            await axios.post("http://localhost:4000/api/v1/message/send",
+            await axios.post("https://hospital-backend-trs5.onrender.com/api/v1/message/send",
                 { firstName, lastName, email, phone, message },
                 {
                     withCredentials: true,
